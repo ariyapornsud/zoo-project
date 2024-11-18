44b9b0969ada6d7584565115fecfeb295879805e
Ariyaporn Sudsatientanone <ariyaporn.suda@kmutt.ac.th>
2024-11-18 19:59:45 +0000

    edit py, test

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,20 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
-    def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
+    def test_negative_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 0)
+
+    def test_age_zero_to_twelve(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_age_thirteen_to_twenty(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+
+    def test_age_twenty_one_to_sixty(self):
+        self.assertEqual(self.zoo.get_ticket_price(30), 150)
+
+    def test_age_over_sixty(self):
+        self.assertEqual(self.zoo.get_ticket_price(65), 100)
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,14 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if age < 0:
+            return 0  # ให้เป็น0เพราะโอกาสน้อยกว่า0ปีเป็นไปได้ยาก
+        elif 0 <= age <= 12:
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20:
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60:
             return 150
-        elif age >= 60:
+        elif age > 60:
             return 100
+        else:
+            return 0 # อายุไม่อยู่ในช่วงที่กำหนด
